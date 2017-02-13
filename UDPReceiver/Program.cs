3a7a1679dd140c364a1a0875269ec53f7d906422
ninjav <alan@ecn.co.za>
2017-02-13 13:10:41 +0200

    Initial commit

---
+++ UDPReceiver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPReceiver
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			UdpClient client = new UdpClient (10000);
+			var endpoint = new IPEndPoint(IPAddress.Any, 11000); 
+			var data = client.Receive(ref endpoint); // listen on port 11000
+
+			Console.WriteLine("receive data from " + endpoint.ToString());
+			Console.WriteLine ("Data: " + Encoding.ASCII.GetString(data));
+		}
+	}
+}
